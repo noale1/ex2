0cad3f5f23825e51746206526e7ce5043aae71d0
noale1 <123noa.levy@gmail.com>
2024-08-28 15:58:17 +0300

    change names and stuff

--- exercise2.py
+++ exercise2.py
@@ -16,42 +16,40 @@ def pop(stack):
 def isEmpty(stack):
     return stack is None
 
-def createNewStack(inputStack):
-    # Extract data into an array format from the input stack
-    dataArray = [data for _, data in inputStack if data is not None]
-
-    # Initialize the output array with 0s and a stack to assist with index calculation
-    outputArray = [0] * len(dataArray)
+def createNewStack(buildingStack):
+    # Change stack into array 
+    dataArray = []
+    for _, data in buildingStack:
+        if data is not None:
+            dataArray.append(data)
+        
+    # Initialize the output array (with zeros) and initialize stack
+    viewArray = []
+    for _ in range(len(dataArray)):
+        viewArray.append(0)
     stack = []
 
-    # Traverse the data_array from left to right
+    # Switch dataArray from left to right
     for i in range(len(dataArray)):
-        # Maintain a stack of indices in decreasing order of their values
         while stack and dataArray[stack[-1]] <= dataArray[i]:
             stack.pop()
-
-        # If stack is not empty, calculate the index for the output_array
         if stack:
-            outputArray[i] = stack[-1] + 1
-
-        # Push current index onto the stack
+            viewArray[i] = stack[-1] + 1
         stack.append(i)
 
-    # Create the output stack based on the output_array
-    outputStack = [(None, outputArray[0])]  # Start with the first node
+    # Create the view stack based on the dataArray
+    viewStack = [(None, viewArray[0])]  
+    for i in range(1, len(viewArray)):
+        viewStack.append((viewArray[i - 1], viewArray[i]))
+    viewStack.append((viewArray[-1], None))  
 
-    for i in range(1, len(outputArray)):
-        outputStack.append((outputArray[i - 1], outputArray[i]))
-
-    outputStack.append((outputArray[-1], None))  # Add the last node
-
-    return outputStack
+    return viewStack
 
 
 def main():
-    inputStack = [(None, 101), (101, 87), (87, 122), (208, 74), (74, 107), (107, 152), (152, 130), (130, None)]
-    outputStack = createNewStack(inputStack)
-    print(outputStack)
+    buildingStack = [(None, 101), (101, 87), (87, 122), (208, 74), (74, 107), (107, 152), (152, 130), (130, None)]
+    viewStack = createNewStack(buildingStack)
+    print(viewStack)
     
 
 if __name__ == "__main__":
