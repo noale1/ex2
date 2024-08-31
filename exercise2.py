dc52c4e1468d7ceb9c78c0d169ae4f9ac801ae0a
noale1 <123noa.levy@gmail.com>
2024-08-31 17:13:24 +0300

    finish this shit

--- exercise2.py
+++ exercise2.py
@@ -13,45 +13,41 @@ def pop(stack):
     stack = stack[1]
     return poppedData, stack
 
+def top(stack):
+    if not isEmpty(stack):
+        return stack[0]
+    return None
+
 def isEmpty(stack):
     return stack is None
 
-def createNewStack(buildingStack):
-    # Change stack into array 
-    dataArray = []
-    for _, data in buildingStack:
-        if data is not None:
-            dataArray.append(data)
+def createViewArray(buildingArray):
+    
+    # Initialize view array with zeros and create an empty stack
+    viewArray = [0] * len(buildingArray)
+    tempStack = []
+    
+    # Go over building list and find the next heighest building
+    # If there is none that means the building's view is the ocean
+    for i in range(len(buildingArray)):
+        while tempStack and buildingArray[top(tempStack)] <= buildingArray[i]:
+            _, tempStack = pop(tempStack)
+            
+        # If the temp stack is not empty, calculate the index for the view array
+        if tempStack:
+            viewArray[i] = top(tempStack) + 1
         
-    # Initialize the output array (with zeros) and initialize stack
-    viewArray = []
-    for _ in range(len(dataArray)):
-        viewArray.append(0)
-    stack = []
-
-    # Switch dataArray from left to right
-    for i in range(len(dataArray)):
-        while stack and dataArray[stack[-1]] <= dataArray[i]:
-            stack.pop()
-        if stack:
-            viewArray[i] = stack[-1] + 1
-        stack.append(i)
-
-    # Create the view stack based on the dataArray
-    viewStack = [(None, viewArray[0])]  
-    for i in range(1, len(viewArray)):
-        viewStack.append((viewArray[i - 1], viewArray[i]))
-    viewStack.append((viewArray[-1], None))  
-
-    return viewStack
-
+        # Push the current index into stack
+        tempStack = push(tempStack, i)
+    
+    return viewArray
 
 def main():
-    buildingStack = [(None, 101), (101, 87), (87, 122), (208, 74), (74, 107), (107, 152), (152, 130), (130, None)]
-    viewStack = createNewStack(buildingStack)
-    print(viewStack)
+    buildingArray = [101, 87, 122, 208, 74, 107, 152, 130]
+    # viewStack = createNewStack(buildingArray)
+    # print(viewStack)
     
+    print(createViewArray(buildingArray))    
 
 if __name__ == "__main__":
     main()
-
